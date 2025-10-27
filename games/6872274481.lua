ec48f047a5fc30e1215e4fec42d4df071de0f2e6
void2realyt <x2voidy09@gmail.com>
2025-10-27 16:43:47 -0400

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12272,27 +12272,3 @@ run(function()
 		Name = 'Camera Direction'
 	})
 end)
-run(function()
-    local Exploit
-
-    Exploit = vape.Categories.Blatant:CreateModule({
-        Name = "Tp Exploit",
-        Function = function(callback)
-            if callback then
-                repeat
-                    for i, v in pairs(game.Players:GetPlayers()) do
-                        if v ~= game.Players.LocalPlayer 
-                        and v.Team ~= game.Players.LocalPlayer.Team 
-                        and v.Character 
-                        and v.Character.PrimaryPart then
-                            v.Character:SetPrimaryPartCFrame(
-                                game.Players.LocalPlayer.Character.PrimaryPart.CFrame * CFrame.new(0, -2, 0)
-                            )
-                        end
-                    end
-                    task.wait()
-                until not callback
-            end
-        end
-    })
-end)
